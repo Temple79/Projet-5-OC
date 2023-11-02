23757e8fd6e2871d059b7a38c8aae5158c300fca
Petar <topchiyski@hotmail.fr>
2023-11-02 10:11:28 +0200

    Correction du script gallery

--- assets/maugallery.js
+++ assets/maugallery.js
@@ -150,12 +150,12 @@
 
       $(imagesCollection).each(function(i) {
         if ($(activeImage).attr("src") === $(this).attr("src")) {
-          index = i ;
+          index = i-1 ;
         }
       });
       next =
         imagesCollection[index] ||
-        imagesCollection[imagesCollection.length - 1];
+        imagesCollection[imagesCollection.length -1];
       $(".lightboxImage").attr("src", $(next).attr("src"));
     },
     nextImage() {
@@ -189,7 +189,7 @@
 
       $(imagesCollection).each(function(i) {
         if ($(activeImage).attr("src") === $(this).attr("src")) {
-          index = i;
+          index = i+1;
         }
       });
       next = imagesCollection[index] || imagesCollection[0];
@@ -239,8 +239,8 @@
       if ($(this).hasClass("active-tag")) {
         return;
       }
-      $(".active-tag").removeClass("active active-tag");
-      $(this).addClass("active-tag");
+      $(".active.active-tag").removeClass("active active-tag");
+      $(this).addClass("active-tag active");
 
       var tag = $(this).data("images-toggle");
 
